bd482ea92c29288917fec8cdebb59549964205f9
parijatsrivastava <parijat.srivastva@outlook.com>
2020-04-15 04:10:31 +0530

    admin login

--- application.py
+++ application.py
@@ -24,6 +24,41 @@ db.init_app(app)
 app.config["SECRET_KEY"] = envs.SECRET_KEY
 socketio = SocketIO(app)
 
+@app.route("/admin")
+def admin():
+    if not session.get("admin"):
+        return redirect(url_for('admin_login'))
+
+    return render_template("admin/homepage.html", shopname=envs.SHOPNAME, admin=session["admin"])
+
+
+@app.route("/admin/login", methods=["POST", "GET"])
+def admin_login():
+    if session.get("admin"):
+        return redirect(url_for('admin'))
+
+    if request.method == "GET":
+        return render_template("admin/login.html", shopname=envs.SHOPNAME)
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    
+    if not username or not password:
+        return render_template("admin/login.html", shopname=envs.SHOPNAME, login_error="Enter Username and Password")
+    
+    if username == envs.ADMIN_USERNAME and password == envs.ADMIN_PASSWORD:
+        session["admin"] = True
+        session.permanent = True
+        return redirect(url_for('admin'))
+    else:
+        return render_template("admin/login.html", shopname=envs.SHOPNAME, login_error="Invalid Username and/or Password")
+
+
+@app.route("/admin/logout")
+def admin_logout():
+    session.clear()
+    return redirect(url_for('admin_login'))
+
 
 @app.route("/")
 def homepage():
---
+++ static/admin/login.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("#loginForm").onsubmit = ()=> {
+        if (!document.querySelector("#username").value) {
+            alert("Enter a Username");
+            return false;
+        }
+        if (!document.querySelector("#password").value) {
+            alert("Enter a Password");
+            return false;
+        }            
+    };
+});
